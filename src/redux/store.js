0c3864af58a643150df42ded5f95c4be792b26e0
Wieczoreks <wieczoreks@hotmail.com>
2020-04-25 18:24:24 +0200

    Removing heroku master

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,11 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore } from "redux-persist";
 
-const middlewares = [logger]
+const middlewares = []
+
+if(process.env.NODE_ENV==='development'){
+    middlewares.push(logger)
+}
 
 export const store  = createStore(rootReducer,  applyMiddleware(...middlewares))
 export const persistor = persistStore(store)
