37002b155474f138ae6fa24216d57594b8df86a9
Wieczoreks <wieczoreks@hotmail.com>
2020-05-02 09:27:20 +0200

    applied container pattern to shop component

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,30 +1,19 @@
 import React, {Component} from 'react';
 import { Route } from 'react-router-dom';
-import CollectionsOverview from '../../components/collections-overview/collections-overview';
-import CollectionPage from '../collection/collection';
-import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/firebase.utils";
+import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
+import CollectionPageContainer from '../collection/collection.container';
 import {connect} from 'react-redux'
-import {updateCollections} from '../../redux/shop/shop.actions'
-import WithSpinner from '../../components/with-spinner/with-spinner'
+import {fetchCollectionsStartAsync} from '../../redux/shop/shop.actions'
 
-const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
-const CollectionPageWithSpinner = WithSpinner(CollectionPage)
+
+
 
 class ShopPage extends Component {
 
-  state = {
-    loading:true
-  }
 
-  unsubscribeFromSnapshot = null
   componentDidMount(){
-    const {updateCollections} = this.props
-    const collectionRef = firestore.collection('collections')
-    collectionRef.onSnapshot(async snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
-      updateCollections(collectionsMap)
-      this.setState({loading:false})
-    })
+   const {fetchCollectionsStartAsync} = this.props
+   fetchCollectionsStartAsync()
   }
 
   componentWillUnmount(){
@@ -33,30 +22,27 @@ class ShopPage extends Component {
 
   render() {
     const { match } = this.props;
-    const { loading } = this.state;
     return (
       <div className='shop-page'>
         <Route
           exact
           path={`${match.path}`}
-          render={props => (
-            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
-          )}
+          component={CollectionsOverviewContainer}
         />
         <Route
           path={`${match.path}/:collectionId`}
-          render={props => (
-            <CollectionPageWithSpinner isLoading={loading} {...props} />
-          )}
+          component={CollectionPageContainer}
         />
       </div>
     );
   }
 }
 
+
+
 const mapDispatchToProps = (dispatch) =>{
   return {
-    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap))
+    fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
   }
 }
 
--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,8 +1,38 @@
-import { UPDATE_COLLECTIONS } from "../types";
+import {  
+    FETCH_COLLECTIONS_FAILURE, 
+    FETCH_COLLECTIONS_START, 
+    FETCH_COLLECTIONS_SUCCESS
+   } from "../types";
+import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/firebase.utils";
 
-export const updateCollections = (collectionsMap) => {
+export const fetchCollectionsSuccess = (collectionsMap) => {
     return {
-        type: UPDATE_COLLECTIONS,
-        payload: collectionsMap
+        type: FETCH_COLLECTIONS_SUCCESS,
+        payload:collectionsMap
+    }
+}
+
+export const fetchCollectionsStart = () => {
+    return {
+        type: FETCH_COLLECTIONS_START,
+    }
+}
+
+export const fetchCollectionsFailure = (errorMessage) => {
+    return {
+        type: FETCH_COLLECTIONS_FAILURE,
+        payload:errorMessage
+    }
+}
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections')
+        dispatch(fetchCollectionsStart())
+        collectionRef.get().then(snapshot => {
+          const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
+          dispatch(fetchCollectionsSuccess(collectionsMap))
+        })
+        .catch(err => dispatch(fetchCollectionsFailure(err.message)))
     }
 }
